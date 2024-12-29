4c8a04dbbc36c3fe65e3521ee271abde9c09f83d
kasekiguchi <kasekiguchi@gmail.com>
2024-12-29 15:17:02 +0900

    tb3

--- 1_launcher/launch_robot.sh
+++ 1_launcher/launch_robot.sh
@@ -4,4 +4,7 @@
 export TURTLEBOT3_MODEL=burger
 #export TURTLEBOT3_MODEL=waffle
 #  empty_world.launch.py")
-$(echo "exec ros2 launch turtlebot3_bringup robot.launch.py")
+# $(echo "exec ros2 launch turtlebot3_bringup robot.launch.py")
+
+export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:/opt/ros/ <ros2-distro >/share/turtlebot3_gazebo/models # Iron and older only with Gazebo Classic
+$(echo "exec ros2 launch nav2_bringup tb3_simulation_launch.py headless:=False")
