a9eeec059c9e6c49f5e232898781fabd006e11df
kasekiguchi <kasekiguchi@gmail.com>
2024-12-29 16:32:27 +0900

    tb3 jazzy

--- 1_launcher/launch_robot.sh
+++ 1_launcher/launch_robot.sh
@@ -1,10 +1,10 @@
 #! /usr/bin/bash
 
 # https://emanual.robotis.com/docs/en/platform/turtlebot3/slam/#run-slam-node
-export TURTLEBOT3_MODEL=burger
+#export TURTLEBOT3_MODEL=burger
 #export TURTLEBOT3_MODEL=waffle
 #  empty_world.launch.py")
 # $(echo "exec ros2 launch turtlebot3_bringup robot.launch.py")
 
-export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:/opt/ros/ <ros2-distro >/share/turtlebot3_gazebo/models # Iron and older only with Gazebo Classic
+#export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:/opt/ros//share/turtlebot3_gazebo/models # Iron and older only with Gazebo Classic
 $(echo "exec ros2 launch nav2_bringup tb3_simulation_launch.py headless:=False")
