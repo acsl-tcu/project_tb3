42749d35d379be7b47dcfd8b3e87a41de3552e66
kasekiguchi <kasekiguchi@gmail.com>
2025-01-11 06:42:50 +0900

    tb3

--- 1_launcher/launch_gazebo.sh
+++ 1_launcher/launch_gazebo.sh
@@ -4,4 +4,6 @@
 export TURTLEBOT3_MODEL=burger
 #export TURTLEBOT3_MODEL=waffle
 #  empty_world.launch.py")
+export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:/opt/ros/$ROS_DISTRO/share/turtlebot3_gazebo/models
+
 $(echo "exec ros2 launch turtlebot3_gazebo turtlebot3_world.launch.py")
