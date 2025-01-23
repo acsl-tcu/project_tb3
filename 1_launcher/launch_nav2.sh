d969b53947aa31cd8fe53311832a3a73db6a1dd5
Kazuma SEKIGUCHI <ksekiguc@tcu.ac.jp>
2025-01-23 10:20:55 +0900

    nav2 getting started

--- 1_launcher/launch_nav2.sh
+++ 1_launcher/launch_nav2.sh
@@ -7,5 +7,5 @@ export TURTLEBOT3_MODEL=burger
 export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:/opt/ros/$ROS_DISTRO/share/turtlebot3_gazebo/models
 
 #$(echo "exec ros2 launch turtlebot3_gazebo turtlebot3_world.launch.py")
-
-$(echo "exec ros2 launch turtlebot3_navigation2 navigation2.launch.py use_sim_time:=True map:=/common/ros_launcher/map")
+ros2 launch nav2_bringup tb3_simulation_launch.py headless:=False
+#$(echo "exec ros2 launch turtlebot3_navigation2 navigation2.launch.py use_sim_time:=True map:=/common/ros_launcher/map")
